303c7a64971ae88f5ba279da6ef6a78efe02cf3d
Sara Oliver <s.oliver1@icloud.com>
2020-02-04 17:14:26 +0000

    add select queries

--- queries.sql
+++ queries.sql
@@ -46,3 +46,24 @@ delete from Genre where Name = 'Traditional';
 
 /* Try to select the record that was just deleted */
 select * from Genre where Name = 'Traditional';
+
+/* Use the Chinook database */
+use Chinook;
+
+/*
+Select the name column for each of the records in
+the `Artist` table
+*/
+select Name from Artist;
+
+/*
+Select the `FirstName` and `LastName` for each of the
+records in the `Customer` table
+*/
+select FirstName, LastName from Customer;
+
+/*
+Select all of the columns for every record in the `Track`
+table
+*/
+select * from Track;
