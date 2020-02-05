a2adf3f8ab7e5aee6872dcec09c5b6a580f2cddf
Sara Oliver <s.oliver1@icloud.com>
2020-02-05 16:06:46 +0000

    adding more challenges for database commands

--- queries.sql
+++ queries.sql
@@ -195,3 +195,23 @@ where the customer's `FirstName` is "Frank"
 */
 SELECT COUNT(*) FROM Customer WHERE FirstName = 'Frank';
 
+
+SELECT COUNT(*) FROM  Track
+GROUP BY AlbumId;
+
+SELECT AlbumId, COUNT(*) FROM Track
+GROUP BY AlbumId;
+
+SELECT Album.Title, COUNT(*) FROM Track
+INNER JOIN Album ON Track.AlbumId = Album.AlbumId
+GROUP BY Track.AlbumId;
+
+SELECT AlbumId, MIN(UnitPrice) FROM Track
+GROUP BY AlbumId;
+
+SELECT AlbumId, MAX(UnitPrice) FROM Track
+GROUP BY AlbumId;
+
+SELECT AlbumId, ROUND(SUM(UnitPrice), 2) FROM Track
+GROUP BY AlbumId;
+
