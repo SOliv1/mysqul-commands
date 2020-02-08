e164489d20b48ac2d953c0405916746823720ea6
Sara Oliver <s.oliver1@icloud.com>
2020-02-08 18:04:45 +0000

    insert and execute many

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,9 +1,10 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get username from workspace if required
 # (modify this variable if running on another environment)
-username = os.getenv('gitpod_USER')
+username = os.getenv('Gitpod_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
@@ -12,12 +13,14 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 # Run queries
 try:
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
-
+    with connection.cursor() as cursor:
+        rows = [("bob", 21, "1990-02-06 23:04:56"),
+                ("jim", 56, "1955-05-09 13:12:45"),
+                ("fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
+        connection.commit()
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
