76f34f3f8ca41ff6ce6f23dd3769fcf281d842b8
Sara Oliver <s.oliver1@icloud.com>
2020-02-08 18:18:38 +0000

    update information

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,10 +14,7 @@ connection = pymysql.connect(host='localhost',
 # Run queries
 try:
     with connection.cursor() as cursor:
-        rows = [("bob", 21, "1990-02-06 23:04:56"),
-                ("jim", 56, "1955-05-09 13:12:45"),
-                ("fred", 100, "1911-09-12 01:01:01")]
-        cursor.executemany("INSERT INTO Friends VALUES (%s,%s,%s);", rows)
+        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
         # Note that the above will still display a warning (not error) if the
         # table already exists
         connection.commit()
