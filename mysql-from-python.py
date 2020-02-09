f2f96416730d2202017fc9652d17af3474ac9040
Sara Oliver <s.oliver1@icloud.com>
2020-02-09 16:10:28 +0000

    delete where in

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,8 +14,12 @@ connection = pymysql.connect(host='localhost',
 # Run queries
 try:
     with connection.cursor() as cursor:
-        rows = cursor.executemany("DELETE FROM Friends WHERE name = %s;", ['bob', 'jim'])
-        connection.commit()
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
         # Note that the above will still display a warning (not error) if the
         # table already exists
         connection.commit()
